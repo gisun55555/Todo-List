fba7411c2dc48a3177100c74accb1e0d7a0a11a3
giseon <pakox123@gmail.com>
2025-01-13 03:58:25 +0900

    feat : 인덱스 페이지 투두리스트 추가

--- todoapp/src/pages/index.tsx
+++ todoapp/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import SearchLayout from '@/\bcomponents/search-layout';
+import TodoContents from '@/\bcomponents/todo-contents';
 
 export default function Home() {
   return (
     <div>
       <SearchLayout></SearchLayout>
+      <TodoContents></TodoContents>
     </div>
   );
 }
