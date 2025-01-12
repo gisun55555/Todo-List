85f11ea3ec6b6a8e691903b1a636819eb21f9973
giseon <pakox123@gmail.com>
2025-01-12 15:30:52 +0900

    style : 프리티어 설정 전체 코드 적용

--- todoapp/src/components/global-layout.tsx
+++ todoapp/src/components/global-layout.tsx
@@ -1,32 +1,26 @@
-import { ReactNode } from "react"
-import style from'./global-layout.module.css'
-import Link from "next/link"
-import Image from "next/image"
-export default function GlobalLayout({
-    children,
-}:{
-    children: ReactNode
-}){
-    return(
-        <div className={style.container}>
+import { ReactNode } from 'react';
+import style from './global-layout.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+export default function GlobalLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className={style.container}>
+      <header className={style.header}>
+        <Link href={'/'}>
+          <div className={style.logo_container}>
+            <Image
+              src="/images/logo.svg"
+              alt="My SVG Image"
+              width={53}
+              height={30}
+            />
+            <div className={style.logo_text}>do it ;</div>
+          </div>
+        </Link>
+      </header>
 
-            <header className={style.header}>
-                <Link href={'/'}>
-                    <div className={style.logo_container}>
-                        <Image 
-                          src="/images/logo.svg" 
-                          alt="My SVG Image" 
-                          width={53} 
-                          height={30} 
-                        />
-                        <div className={style.logo_text}>do it ;</div>
-                  
-                    </div>
-                    </Link>
-                </header>
-                
-            <main className={style.main}>내용 넣을예정요{children}</main>
-            {/* <footer>2</footer> */}
-        </div>
-    )
+      <main className={style.main}>내용 넣을예정요{children}</main>
+      {/* <footer>2</footer> */}
+    </div>
+  );
 }
--- todoapp/src/pages/_app.tsx
+++ todoapp/src/pages/_app.tsx
@@ -1,12 +1,11 @@
-import GlobalLayout from "@/\bcomponents/global-layout";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import GlobalLayout from '@/\bcomponents/global-layout';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <GlobalLayout>
-   <Component {...pageProps} />
-  </GlobalLayout>
-)
-  ;
+    <GlobalLayout>
+      <Component {...pageProps} />
+    </GlobalLayout>
+  );
 }
